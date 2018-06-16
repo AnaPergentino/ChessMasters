4b719eece1865f4c96f0e73da220aa8a0b1e87e3
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2018-06-16 12:06:22 -0300

    Cria estrutura básica do projeto

---
+++ src/test_board.cpp
@@ -0,0 +1,8 @@
+/**
+ * Implementa os testes de representação do tabuleiro
+ */
+
+#include "includes/catch.hpp"
+#include "includes/arvore.h"
+#include <string.h>
+
---
+++ src/test_main.cpp
@@ -0,0 +1,6 @@
+/**
+ * Implementa função principal dos testes
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "includes/catch.hpp"
